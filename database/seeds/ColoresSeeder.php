1eca67c31bfb3c86101f42066dc63d701f579980
Laymont Arratia <laymont@gmail.com>
2018-06-13 23:49:37 -0400

    Logica 360°

---
+++ database/seeds/ColoresSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ColoresSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('colores')->insert([
+        ['nombre' => 'Blanco'],
+        ['nombre' => 'Blanco brillante'],
+        ['nombre' => 'Canela'],
+        ['nombre' => 'Ceniza'],
+        ['nombre' => 'Espresso'],
+        ['nombre' => 'Gris'],
+        ['nombre' => 'Gris claro'],
+        ['nombre' => 'Gris Plomo'],
+        ['nombre' => 'inoxidable'],
+        ['nombre' => 'Lino'],
+        ['nombre' => 'Madera'],
+        ['nombre' => 'Manzano'],
+        ['nombre' => 'Marrón'],
+        ['nombre' => 'Mate'],
+        ['nombre' => 'Negro'],
+        ['nombre' => 'Roble'],
+        ['nombre' => 'Rovere mate'],
+        ['nombre' => 'Tabaco']
+      ]);
+
+    }
+}
