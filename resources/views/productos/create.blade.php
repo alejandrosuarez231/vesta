f6b8c9a037b7fa0c2cc7d684fea7901c363df2ae
Laymont Arratia <laymont@gmail.com>
2018-06-21 00:58:42 -0400

    Renew Create

--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -9,7 +9,7 @@
   </div>
 
   <div class="row">
-    <div class="col-md-7">
+    <div class="col-md">
       <div class="form-row">
         <div class="form-group mr-2">
           {!! Form::label('sku', null, ['class'=>'form-control-label']) !!}
@@ -34,7 +34,7 @@
       <!-- Morph to be MTP or PSE -->
       <div class="form-group" v-if="tipo_id != 8">
         {!! Form::label('nombre', 'Nombre', ['class'=>'form-control-label']) !!}
-        {!! Form::text('nombre', null, ['class'=>'form-control col-md-5', 'autocomplete'=>'off' ,'v-model'=>'nombre']) !!}
+        {!! Form::text('nombre', null, ['class'=>'form-control col-md-5 text-uppercase', 'autocomplete'=>'off' ,'v-model'=>'nombre']) !!}
         {!! $errors->first('nombre', '<small class="help-block text-danger">:message</small>') !!}
       </div>
       <!-- Morph to be MTP or PSE -->
@@ -50,7 +50,7 @@
         {!! $errors->first('descripcion', '<small class="help-block text-danger">:message</small>') !!}
       </div>
 
-      <div class="form-row">
+      <div class="form-row" v-if="proppto == false && proppse == false ">
         <div class="form-group mr-2">
           {!! Form::label('marca_id', 'Marca', ['class'=>'form-control-label']) !!}
           {!! Form::select('marca_id', \App\Marca::pluck('nombre','id'), null, ['class'=>'form-control','placeholder'=>'Selección','v-model'=>'marca_id']) !!}
@@ -63,63 +63,127 @@
 
       {{-- Propiedades Materia Prima --}}
       <div class="form-row mb-2">
-        <div class="from-group mr-2">
-          {!! Form::text('largo', null, ['class'=>'form-control','step'=>'any', 'v-model' => 'largo','placeholder'=>'Largo']) !!}
+        <legend>Propiedades</legend>
+        <div class="from-group mr-2 mb-2">
+          {!! Form::text('largo', null, ['class'=>'form-control text-uppercase','step'=>'any', 'v-model' => 'largo','placeholder'=>'Largo']) !!}
           {!! $errors->first('largo', '<small class="help-block text-danger">:message</small>') !!}
         </div>
-        <div class="from-group mr-2">
-          {!! Form::text('ancho', null, ['class'=>'form-control','step'=>'any', 'v-model' => 'ancho','placeholder'=>'Ancho']) !!}
+        <div class="from-group mr-2 mb-2">
+          {!! Form::text('ancho', null, ['class'=>'form-control text-uppercase','step'=>'any', 'v-model' => 'ancho','placeholder'=>'Ancho']) !!}
           {!! $errors->first('ancho', '<small class="help-block text-danger">:message</small>') !!}
         </div>
-        <div class="from-group mr-2">
-          {!! Form::text('area', null, ['class'=>'form-control','step'=>'any', 'v-model' => 'area','placeholder'=>'Area']) !!}
+        <div class="from-group mr-2 mb-2">
+          {!! Form::text('profundidad', null, ['class'=>'form-control text-uppercase','step'=>'any', 'v-model' => 'profundidad','placeholder'=>'Profundidad']) !!}
+          {!! $errors->first('profundidad', '<small class="help-block text-danger">:message</small>') !!}
+        </div>
+        <div class="from-group mr-2 mb-2" v-if="proppto == false">
+          {!! Form::text('area', null, ['class'=>'form-control text-uppercase','step'=>'any', 'v-model' => 'area','placeholder'=>'Area']) !!}
           {!! $errors->first('area', '<small class="help-block text-danger">:message</small>') !!}
         </div>
-        <div class="from-group mr-2">
-          {!! Form::text('espesor', null, ['class'=>'form-control','step'=>'any', 'v-model' => 'espesor','placeholder'=>'Espesor']) !!}
+        <div class="from-group mr-2 mb-2" v-if="proppto == false">
+          {!! Form::text('espesor', null, ['class'=>'form-control text-uppercase','step'=>'any', 'v-model' => 'espesor','placeholder'=>'Espesor']) !!}
           {!! $errors->first('espesor', '<small class="help-block text-danger">:message</small>') !!}
         </div>
       </div>
-    </div><!-- col-md-8 -->
+    </div><!-- col-md -->
+    <div class="col-md border border-info rounded mb-2" style="background-color: #e0f7fa;" v-if="proppto == true">
+      <div class="form-group p-2">
+        {!! Form::checkbox('combo', null, null, ['class'=>'form-check-input','v-model'=>'combo']) !!}
+        {!! Form::label('combo', 'Combo/Compuesto', ['class'=>"form-check-label font-weight-bold"]) !!}
+      </div>
+      <div class="form-group mb-1 p-2" v-if="combo == true">
+        <legend>PTO's</legend>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>SKU</th>
+              <th>Descripción</th>
+              <th>Cantidad</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>data</td>
+              <td>data</td>
+              <td>data</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <legend>MTP's</legend>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>SKU</th>
+            <th>Descripción</th>
+            <th colspan="2">Cantidad</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(mtp, $index) in mtps" track-by="$index">
+            <td> @{{ $index +1 }} </td>
+            <td>
+              <input name="" class="form-control-plaintext text-right text-uppercase" type="text" v-model="mtp.sku" placeholder="SKU">
+            </td>
+            <td>
+              {!! Form::select('mtp_producto_id', \App\Producto::where('tipo_id','<',8)->pluck('nombre','id'), null, ['class'=>'form-control','placeholder'=>'Selección','v-model'=>'mtp.mtp_producto_id']) !!}
+            </td>
+            <td>
+              <input name="" class="form-control-plaintext text-right" type="number" v-model="mtp.cantidad" placeholder="Cantidad">
+            </td>
+            <td>
+              <small>
+                <span class="text-primary font-weight-bold" @click="addRowMTP($index)" title="Add"><i class="fas fa-plus"></i></span>
+                <span class="text-danger font-weight-bold" @click="removeRowMTP($index)" v-if="mtps.length > 1" title="Remove"><i class="fas fa-minus"></i></span>
+              </small>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 
   <div class="row" v-if="proppse == true">
     <div class="col-md-6">
+      <legend>Propiedades Extra</legend>
       <div class="form-row">
         <div class="form-group mr-2">
-          {!! Form::text('veta', null, ['class'=>'form-control','placeholder'=>'Veta']) !!}
+          {!! Form::text('veta', null, ['class'=>'form-control text-uppercase','placeholder'=>'Veta']) !!}
         </div>
         <div class="form-group mr-2">
-          {!! Form::text('material', null, ['class'=>'form-control','placeholder'=>'Material']) !!}
+          {!! Form::text('material', null, ['class'=>'form-control text-uppercase','placeholder'=>'Material']) !!}
         </div>
         <div class="form-group mr-2">
-          {!! Form::text('largo_izq', null, ['class'=>'form-control','placeholder'=>'Largo Izq.']) !!}
+          {!! Form::text('largo_izq', null, ['class'=>'form-control text-uppercase','placeholder'=>'Largo Izq.']) !!}
         </div>
         <div class="form-group mr-2">
-          {!! Form::text('largo_der', null, ['class'=>'form-control','placeholder'=>'Largo Der.']) !!}
+          {!! Form::text('largo_der', null, ['class'=>'form-control text-uppercase','placeholder'=>'Largo Der.']) !!}
         </div>
         <div class="form-group mr-2">
-          {!! Form::text('ancho_sup', null, ['class'=>'form-control','placeholder'=>'Ancho Sup.']) !!}
+          {!! Form::text('ancho_sup', null, ['class'=>'form-control text-uppercase','placeholder'=>'Ancho Sup.']) !!}
         </div>
         <div class="form-group mr-2">
-          {!! Form::text('ancho_inf', null, ['class'=>'form-control','placeholder'=>'Ancho Inf.']) !!}
+          {!! Form::text('ancho_inf', null, ['class'=>'form-control text-uppercase','placeholder'=>'Ancho Inf.']) !!}
         </div>
         <div class="form-group mr-2">
-          {!! Form::text('mec1', null, ['class'=>'form-control','placeholder'=>'Mec 1']) !!}
+          {!! Form::text('mec1', null, ['class'=>'form-control text-uppercase','placeholder'=>'Mec 1']) !!}
         </div>
         <div class="form-group mr-2">
-          {!! Form::text('mec2', null, ['class'=>'form-control','placeholder'=>'Mec 2.']) !!}
+          {!! Form::text('mec2', null, ['class'=>'form-control text-uppercase','placeholder'=>'Mec 2.']) !!}
         </div>
       </div>
     </div>
   </div>
 
-  <div class="row" v-if="proppto == true"><!--Modulos para Producto terminado -->
+  <div class="row" style="background-color: #fffde7;" v-if="proppto == true"><!--Modulos para Producto terminado -->
     <div class="col-md-10">
+      <legend>Partes</legend>
       <div class="form-group">
         <table class="table">
           <thead>
             <tr>
+              <th>#</th>
               <th>Modulo</th>
               <th>SKU</th>
               <th>Letra</th>
@@ -129,12 +193,28 @@
               <th>Ancho</th>
               <th>Material</th>
               <th>Mec1</th>
-              <th>Mec2</th>
+              <th colspan="2">Mec2</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              td*10>input
+            <tr v-for="(item, $index) in pto_pse" track-by="$index">
+              <td>@{{ $index +1 }}</td>
+              <td><input name="modulo" class="form-control-plaintext text-uppercase" type="text" v-model="item.modulo" placeholder="Modulo"></td>
+              <td><input name="sku" class="form-control-plaintext text-uppercase" type="text" v-model="item.sku" placeholder="SKU"></td>
+              <td><input name="letra" class="form-control-plaintext text-uppercase" type="text" v-model="item.letra" placeholder="Letra"></td>
+              <td><input name="descripcion" class="form-control-plaintext text-uppercase" type="text" v-model="item.descripcion" placeholder="Descripción"></td>
+              <td><input name="cantidad" class="form-control-plaintext text-uppercase" type="text" v-model="item.cantidad" placeholder="Cantidad"></td>
+              <td><input name="largo" class="form-control-plaintext text-uppercase" type="text" v-model="item.largo" placeholder="Largo"></td>
+              <td><input name="ancho" class="form-control-plaintext text-uppercase" type="text" v-model="item.ancho" placeholder="Ancho"></td>
+              <td><input name="material" class="form-control-plaintext text-uppercase" type="text" v-model="item.material" placeholder="Material"></td>
+              <td><input name="mec1" class="form-control-plaintext text-uppercase" type="text" v-model="item.mec1" placeholder="Mec1"></td>
+              <td><input name="mec2" class="form-control-plaintext text-uppercase" type="text" v-model="item.mec2" placeholder="Mec2"></td>
+              <td>
+                <small>
+                  <span class="text-primary font-weight-bold" @click="addRow($index)" title="Add"><i class="fas fa-plus"></i></span>
+                  <span class="text-danger font-weight-bold" @click="removeRow($index)" v-if="pto_pse.length > 1" title="Remove"><i class="fas fa-minus"></i></span>
+                </small>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -161,6 +241,7 @@
       color_id: '',
       largo: '',
       ancho: '',
+      profundidad: '',
       area: '',
       espesor: '',
       importado: 0,
@@ -170,18 +251,14 @@
       proppse: false,
       proppto: false,
       pto_pse: [
-        { modulo: '' },
-        { sku: '' },
-        { letra: '' },
-        { descripcion: '' },
-        { cantidad: ' ' },
-        { largo: '' },
-        { ancho: '' },
-        { material: '' },
-        { mec1: '' },
-        { mec2: '' }
-      ]
+      { modulo: '', sku: '', letra: '', descripcion: '', cantidad: ' ', largo: '', ancho: '', material: '', mec1: '', mec2: '' }
+      ],
+      letras: [ { letra: 'A' }, { letra: 'B' }, { letra: 'C' }, { letra: 'D' }, { letra: 'E' }, { letra: 'F' }, { letra: 'G' }, { letra: 'H' }, { letra: 'I' }, { letra: 'J' }, { letra: 'K' }, { letra: 'L' }, { letra: 'M' }, { letra: 'N' }, { letra: 'O' }, { letra: 'P' }, { letra: 'Q' }, { letra: 'R' }, { letra: 'S' }, { letra: 'T' }, { letra: 'U' }, { letra: 'V' }, { letra: 'W' }, { letra: 'X' }, { letra: 'Y' }, { letra: 'Z' } ],
+      combo: false,
+      mtps: [ { sku: '', mtp_producto_id: '', cantidad: '' }],
     },
+
+    computed: {},
 
     methods: {
       getSTipos: function(){
@@ -192,6 +269,35 @@
         .catch( function(error) {
           console.log(error)
         })
+      },
+      addRow: function (index) {
+        try {
+          this.pto_pse.splice(index + 1, 0, {});
+        } catch(e)
+        {
+          console.log(e);
+        }
+      },
+      removeRow: function(index){
+        console.log(index);
+        if( this.pto_pse.length > 1){
+          this.pto_pse.splice(index, 1);
+        }
+      },
+      /* MTP's */
+      addRowMTP: function (index) {
+        try {
+          this.mtps.splice(index + 1, 0, {});
+        } catch(e)
+        {
+          console.log(e);
+        }
+      },
+      removeRowMTO: function(index){
+        console.log(index);
+        if( this.mtps.length > 1){
+          this.mtps.splice(index, 1);
+        }
       }
     },
 
