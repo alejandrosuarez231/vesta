c8b623bf9f63e6ced2f85bc4e0630a6cdc6ceab2
Laymont Arratia <laymont@gmail.com>
2018-09-08 00:19:39 -0400

    Seeder Materiales

--- database/seeds/Materiales.php
+++ database/seeds/Materiales.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+use App\Materiale;
 
 class Materiales extends Seeder
 {
@@ -12,13 +12,15 @@ class Materiales extends Seeder
      */
     public function run()
     {
-        DB::table('materiales')->insert([
-        ['nombre' => 'Caja'],
-        ['nombre' => 'Frente'],
-        ['nombre' => 'Fondo'],
-        ['nombre' => 'Gaveta'],
-        ['nombre' => 'Hielo Seco'],
-        ['nombre' => 'Sobre']
-      ]);
+        $materiale = collect([
+            ['id' => 1, 'tipos' => '14,15,16,17', 'subtipos' =>  '41,42,43,45,46,47,48,49,50,51,52,53,54,55,56', 'sku' => NULL, 'nombre' => 'Caja'],
+            ['id' => 2, 'tipos' => '14,15,16,17', 'subtipos' =>  '41,42,43,45,46,47,48,49,50,51,52,53,54,55,56', 'sku' => NULL, 'nombre' => 'Frente'],
+            ['id' => 3, 'tipos' => '14,15,16,17', 'subtipos' =>  '41,42,43,45,46,47,48,49,50,51,52,53,54,55,56', 'sku' => NULL, 'nombre' => 'Fondo'],
+            ['id' => 4, 'tipos' => '14', 'subtipos' => '44', 'sku' => NULL, 'nombre' => 'Gaveta'],
+            ['id' => 5, 'tipos' => '14,15,16,17', 'subtipos' =>  '41,42,43,45,46,47,48,49,50,51,52,53,54,55,56', 'sku' => NULL, 'nombre' => 'Hielo Seco'],
+            ['id' => 6, 'tipos' => '14,15,16,17', 'subtipos' => NULL, 'sku' => NULL, 'nombre' => 'Sobre'],
+            ['id' => 7, 'tipos' => '14', 'subtipos' => '41,42,43,45', 'sku' => NULL, 'nombre' => 'Gavetas']
+
+        ]);
     }
 }
