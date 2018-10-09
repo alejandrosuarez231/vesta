8b15e482ffb5e241b010fccaf022458b7e007de6
Laymont Arratia <laymont@gmail.com>
2018-10-09 13:44:36 -0400

    Connect ZohoCRM-VestaERP

--- app/Http/Controllers/ZohoController.php
+++ app/Http/Controllers/ZohoController.php
@@ -9,19 +9,20 @@ class ZohoController extends Controller
 {
   public function clientIndex()
   {
-    $clientes = new ZohoCRMClient('clients', 'f05fb8667500b7f823b2e664c6ff0529');
-    dd($clientes);
-    $resultados = $clientes->getRecords()
-          ->selectColumns('First Name', 'Last Name', 'Email')
-          ->sortBy('Last Name')->sortAsc()
-          ->since(date_create('last week'))
-          ->fromIndex(1)
-          ->toIndex(200)
-          ->request();
+    $client = new ZohoCRMClient('Products', '0615079017f7956b1d47442349b17804');
+    $records = $client->getRecords()
+    // ->selectColumns('Account Owner', 'Account Name', 'Correo Electrónico', 'Estatus de Cliente')
+    // ->sortBy('País')->sortAsc()
+    // ->since(date_create('last week'))
+    ->fromIndex(1)
+    ->toIndex(200)
+    ->request();
 
-    // Just for debug
-    echo "<pre>";
-    print_r($resultados);
-    echo "</pre>";
+      // Getting the data
+    foreach ($records as $record) {
+      echo "<pre>";
+      print_r($record->getData());
+      echo "</pre>";
+    }
   }
 }
